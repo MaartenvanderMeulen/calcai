38791441443f6135e08735de4b0bd39ac2497bdd
MaartenvanderMeulen <maarten.vandermeulen@gmail.com>
2020-03-15 20:01:18 +0100

    reads problems from stdin now.  Limit that it can solve is easy2.txt = (A+1)(B+2)(C+3) + ABC

--- README.txt
+++ README.txt
@@ -1,1 +1,24 @@
-python calcai.py
+python calcai.py < easy2.txt
+
+easy2 is: (A+1)(B+2)(C+3) + ABC = 2ABC + 3AB + 2AC + BC + 6A + 3B + 2C + 6
+
+hop 1, failed
+hop 2, failed
+hop 3, failed
+hop 4, failed
+hop 5, failed
+hop 6, solved: add(sub(sub(add(mul(add(add(B, 4), sub(A, B)), add(C, B)), mul(add(A, B), C)), sub(sub(sub(sub(add(B, B), sub(mul(A, add(B, B)), C)), add(add(A, add(A, add(sub(B, C), sub(2, B)))), add(A, B))), add(add(add(B, 4), sub(A, B)), add(A, B))), A)), B), mul(mul(A, add(B, B)), C))
+hop 7, failed
+hop 8, solved: add(mul(mul(B, C), add(A, A)), add(mul(C, add(A, A)), add(mul(add(add(A, add(A, A)), C), B), add(add(add(A, add(A, add(A, add(A, 6)))), C), add(add(B, add(add(B, add(A, B)), A)), C)))))
+hop 9, solved: add(mul(mul(B, A), add(C, add(4, C))), add(add(mul(C, B), mul(A, C)), add(add(A, add(add(5, add(add(A, add(A, add(mul(A, C), A))), add(add(mul(B, 2), sub(add(A, sub(A, 5)), mul(B, A))), C))), mul(1, B))), add(C, 6))))
+hop 10, failed
+hop 11, failed
+hop 12, failed
+hop 13, failed
+hop 14, failed
+hop 15, failed
+hop 16, failed
+hop 17, failed
+hop 18, failed
+hop 19, failed
+hop 20, solved: sub(mul(mul(C, A), add(add(3, B), B)), sub(add(A, add(add(B, add(add(sub(sub(B, add(A, 6)), C), add(add(add(add(B, B), sub(sub(B, A), C)), add(add(sub(add(B, B), mul(sub(3, C), sub(A, B))), B), sub(B, A))), B)), sub(B, A))), B)), mul(add(A, 6), add(add(B, B), B))))
--- calcai.py
+++ calcai.py
@@ -1,90 +1,104 @@
 # Inspired by https://deap.readthedocs.io/en/master/examples/gp_symbreg.html
 # That example has extensive explanations.
+import sys
 import operator
 import math
 import random
-
 import numpy
-
 from deap import algorithms
 from deap import base
 from deap import creator
 from deap import tools
 from deap import gp
 
-# Define new functions
-def protectedDiv(left, right):
+def protected_div(left, right):
+    '''division operator that has a normal result when dividing by zero'''
     try:
         return left / right
     except ZeroDivisionError:
         return 0 
-
-pset = gp.PrimitiveSet("MAIN", 3)
-pset.addPrimitive(operator.add, 2)
-pset.addPrimitive(operator.sub, 2)
-pset.addPrimitive(operator.mul, 2)
-pset.addPrimitive(protectedDiv, 2)
-pset.addPrimitive(operator.neg, 1)
-pset.addPrimitive(math.cos, 1)
-pset.addPrimitive(math.sin, 1)
-pset.addEphemeralConstant("rand101", lambda: random.randint(-1,1))
-pset.renameArguments(ARG0='A')
-pset.renameArguments(ARG1='B')
-pset.renameArguments(ARG2='C')
-
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
-
-toolbox = base.Toolbox()
-toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
-toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-toolbox.register("compile", gp.compile, pset=pset)
-
-def rmse(examples, individual):
+        
+        
+def rmse(toolbox, individual):
     # Transform the tree expression in a callable function
     model = toolbox.compile(expr=individual)
-    # Evaluate the root of mean squared error (RMSE)
-    se = [(model(x[0], x[1], x[2]) - y)**2 for x, y in examples]
-    rmse = math.sqrt(sum(se) / len(examples))
+    # Evaluate with the root of mean squared error (RMSE)
+    se = [(model(x[0], x[1], x[2]) - y)**2 for x, y in toolbox.examples]
+    rmse = math.sqrt(sum(se) / len(toolbox.examples))
     return rmse
 
-def evalSymbReg(examples, individual):    
+
+def evaluate(toolbox, individual):    
     penalty = len(str(individual)) / 1000 # Penalty for solutions that are longer than needed
-    return rmse(examples, individual) + penalty,
+    return rmse(toolbox, individual) + penalty,
 
-examples = []
-for i in range(10):
-    x0, x1, x2 = random.randint(1,9), random.randint(1,9), random.randint(1,9)
-    y = x0 * (x1 + x2 + 1)
-    examples.append(((x0, x1, x2), y))
-print("examples", examples)
-toolbox.register("evaluate", evalSymbReg, examples)
-toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("mate", gp.cxOnePoint)
-toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
-toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
 
-toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
-toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+def get_examples():
+    examples = []
+    hdr = sys.stdin.readline()
+    for line in sys.stdin:
+        x0, x1, x2, y = (float(s) for s in line.split("\t"))
+        examples.append(((x0, x1, x2), y))
+    # print(f"{len(examples)} examples, the last is", examples[-1])
+    return examples
 
-def main():
-    random.seed(318)
 
-    pop = toolbox.population(n=300)
-    hof = tools.HallOfFame(1)
+def initialize_genetic_programming_toolbox(examples):
+    pset = gp.PrimitiveSet("MAIN", 3)
+    pset.addPrimitive(operator.add, 2)
+    pset.addPrimitive(operator.mul, 2)
+    pset.addPrimitive(operator.sub, 2)
+    # pset.addPrimitive(protected_div, 2)
+    # pset.addPrimitive(operator.neg, 1)
+    # pset.addPrimitive(math.cos, 1)
+    # pset.addPrimitive(math.sin, 1)
+    pset.addEphemeralConstant("randint", lambda: random.randint(1,9))
+    pset.renameArguments(ARG0='A')
+    pset.renameArguments(ARG1='B')
+    pset.renameArguments(ARG2='C')
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+    toolbox = base.Toolbox()
+    toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=3)
+    toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    toolbox.register("compile", gp.compile, pset=pset)
+    toolbox.examples = examples
+    toolbox.register("evaluate", evaluate, toolbox)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+    toolbox.register("mate", gp.cxOnePoint)
+    toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
+    toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+    toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+    toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+    return toolbox
     
+
+def calc_ai(toolbox):
+    random.seed(318)
+    pop = toolbox.population(n=300)
+    hof = tools.HallOfFame(1)    
     stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
     stats_size = tools.Statistics(len)
     mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
     mstats.register("min", numpy.min)
-
-    pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 40, stats=mstats,
+    _, _ = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 200, stats=mstats,
                                    halloffame=hof, verbose=False)
-    optimum = "mul(A, add(B, add(C, 1)))"
-    print("solution", str(hof[0]))
-    print(f"rmse {rmse(examples, hof[0])}, solution length {len(str(hof[0]))}, optimal solution length {len(optimum)}")
-    return pop, log, hof
+    return hof[0]
+
+
+def main():
+    examples = get_examples()
+    toolbox = initialize_genetic_programming_toolbox(examples)
+    for i in range(20):
+        solution = calc_ai(toolbox)
+        solution_str = str(solution)
+        error = rmse(toolbox, solution)
+        if error == 0:
+            print(f"hop {i+1}, solved: {solution_str}")
+        else:
+            print(f"hop {i+1}, failed")
+
 
 if __name__ == "__main__":
     main()
